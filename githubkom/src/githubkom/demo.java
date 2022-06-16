668f9a5867598257c19c1e0a01fbc167a23d83e0
komal140792 <107626591+komal140792@users.noreply.github.com>
2022-06-16 20:52:21 +0530

    Update demo.java

--- githubkom/src/githubkom/demo.java
+++ githubkom/src/githubkom/demo.java
@@ -6,6 +6,15 @@ public class demo
 	public static void main(String[] args)
 	{
 	
-		System.out.println("my name is komal Abhi mad");
+		System.out.println("my name is komal Abhi madane");
 	}
+	public void m1()
+	{
+			System.out.println("my address phaltan");
+
+	
+	}
+	
+	
+	
 }
